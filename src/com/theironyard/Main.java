4f9cf21789b99421259808b7807a4d8e92622e49
michaeldelli-gatti <michaelddg@gmail.com>
2016-06-08 19:20:44 -0400

    add previous, next, list of 20, clicking names almost works

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,8 +1,13 @@
 package com.theironyard;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -25,6 +30,48 @@ public class Main {
 
         parsePeople();
 
+        Spark.init();
+        Spark.get(
+                "/",
+                (request, response) -> {
+                    String offsetStr = request.queryParams("offset");
+                    int offset = 0;
+                    boolean hasPrevious = false;
+                    boolean hasNext = true;
+                    if (offsetStr != null) {
+                        offset = Integer.valueOf(offsetStr);
+                        if (offset <= 0) {
+                            hasPrevious = false;
+                        }
+                        else{ hasPrevious = true;}
+                        if (offset+20 >= 1000){
+                            hasNext = false;
+                        }
+                        else{ hasNext = true;}
+                    }
+                    ArrayList p = new ArrayList<>(people.subList(offset, offset + 20));
+                    HashMap m = new HashMap();
+                    m.put("people", p);
+                    m.put("offsetNext", offset + 20);
+                    m.put("offsetPrevious", offset - 20);
+                    m.put("hasPrevious", hasPrevious);
+                    m.put("hasNext", hasNext);
+                    return new ModelAndView(m, "home.html");
+                },
+                new MustacheTemplateEngine()
+        );
+        Spark.get(
+                "/person",
+                (request, response) -> {
+                    HashMap m = new HashMap<>();
+                    int idNum = Integer.valueOf(request.queryParams("id"));
+                    Person p = people.get(idNum - 1);
+                    m.put("id", p.id);
+                    return new ModelAndView(m, "person.html");
+                },
+                new MustacheTemplateEngine()
+        );
 
+        System.out.println("FUCK");
     }
 }
